2babd155dd97b590c7be2194fd0aaee1dd414758
SusanaMarin2004 <119801310+SusanaMarin2004@users.noreply.github.com>
2022-12-12 13:10:29 -0500

    Add files via upload

---
+++ comic/escena 16/interacciones.js
@@ -0,0 +1,19 @@
+var activarinter = document.querySelector(".circulo")
+
+var interaccion = document.querySelector(".texto")
+
+var esquizo = document.querySelector(".tension")
+
+activarinter.addEventListener("click", inter)
+
+function inter(){
+
+    esquizo.setAttribute("src", "./media/sfx/suspenso.mp3")
+    esquizo.play()
+
+    activarinter.classList.add("oculto");
+
+    activarinter.classList.remove("titilar");
+
+    interaccion.classList.add("desaparicion");
+}
